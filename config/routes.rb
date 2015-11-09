39b4c535b5ba63ef56585d028e7f606f9f8a9f68
Sean M. Snyder <sean.snyder@snhu.edu>
2015-11-08 21:40:37 -0500

    change route root

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :caves
   resources :creatures
-  if Rails.env.development?
-    root 'home#index'
-  else
-    root 'caves#index'
-  end
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
