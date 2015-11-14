cdd089e53b1b74e08e37dad4d3d3d8c220fddb8c
Sean M. Snyder <sean.snyder@snhu.edu>
2015-11-13 22:11:53 -0500

    grid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
 	:address				=>	'smtp.sendgrid.net',
 	:port					=>	'587',
 	:authentication			=>	:plain,
-	:user_name				=>	'app43599142@heroku.com',
-	:password				=>	'c8mqztfu1698',
+	:user_name				=>	ENV['SENDGRID_USERNAME'],
+	:password				=>	ENV['SENDGRID_PASSWORD'],
 	:domain					=> 'heroku.com',
 	:enable_starttls_auto   => 'true'
 }
