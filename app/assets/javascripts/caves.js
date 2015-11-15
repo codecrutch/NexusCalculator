fe18abf8308dfc23105ae6b00a2dd7edffc1c8ab
Sean M. Snyder <sean.snyder@snhu.edu>
2015-11-14 21:22:53 -0500

    javascript for hiding caves

--- app/assets/javascripts/caves.js
+++ app/assets/javascripts/caves.js
@@ -1,10 +1,39 @@
 $(document).ready(function() {
+	$("#assassin-toggle").click(function(){
+		$("#assassin-creatures").fadeToggle();
+	})
+	$("#magus-toggle").click(function(){
+		$("#magus-creatures").fadeToggle();
+	})
+	$("#hunter-toggle").click(function(){
+		$("#hunter-creatures").fadeToggle();
+	})
+	$("#hillman-toggle").click(function(){
+		$("#hillman-creatures").fadeToggle();
+	})
 	$("#anchorite-toggle").click(function(){
 		$("#anchorite-creatures").fadeToggle();
 	})
-
-  	$("#magus-toggle").click(function(){
-		$("#magus-creatures").fadeToggle();
+	$("#wind-toggle").click(function(){
+		$("#wind-creatures").fadeToggle();
+	})
+  	$("#shadow-toggle").click(function(){
+		$("#shadow-creatures").fadeToggle();
+	})
+	$("#bandit-toggle").click(function(){
+		$("#bandit-creatures").fadeToggle();
+	})
+	$("#dread-toggle").click(function(){
+		$("#dread-creatures").fadeToggle();
+	})
+	$("#greyhand-toggle").click(function(){
+		$("#greyhand-creatures").fadeToggle();
+	})
+	$("#earth-toggle").click(function(){
+		$("#earth-creatures").fadeToggle();
+	})
+	$("#water-toggle").click(function(){
+		$("#water-creatures").fadeToggle();
 	})
 });
  
